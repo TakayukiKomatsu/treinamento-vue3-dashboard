fb33bc043a7c7e6f258043be36967e5d2cdad987
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-02-14 23:32:20 -0300

    feat: modal login

--- src/components/ModalLogin/index.vue
+++ src/components/ModalLogin/index.vue
@@ -1,9 +1,92 @@
 <template>
-  <h1>
-    Modal de Login
-  </h1>
+  <div class="flex justify-between">
+    <h1 class="text-4xl font-black text-gray-800">Entre na sua conta</h1>
+
+    <button class="text-4xl text-gray-600 focus:outline-none" @click="close">
+      &times;
+    </button>
+  </div>
+
+  <div class="mt-16">
+    <form @submit.prevent="handleSubmit">
+      <label class="block">
+        <span class="text-lg font-medium text-gray-800">E-mail</span>
+        <input
+          type="email"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rouded"
+          :class="{
+            'border-brand-danger': !!state.email.errorMessage
+          }"
+          placeholder="jane.doe@gmail.com"
+          v-model="state.email.value"
+        />
+        <span
+          class="block font-medium text-brand-danger"
+          v-if="!!state.email.errorMessage"
+        >
+          {{ state.email.errorMessage }}
+        </span>
+      </label>
+
+      <label class="block">
+        <span class="text-lg font-medium text-gray-800">E-mail</span>
+        <input
+          type="password"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rouded"
+          :class="{
+            'border-brand-danger': !!state.password.errorMessage
+          }"
+          placeholder="jane.doe@gmail.com"
+          v-model="state.password.value"
+        />
+        <span
+          class="block font-medium text-brand-danger"
+          v-if="!!state.password.errorMessage"
+        >
+          {{ state.password.errorMessage }}
+        </span>
+      </label>
+
+      <button
+        :disabled="state.isLoading"
+        type="submit"
+        :class="{
+          'opacity-50': state.isLoading
+        }"
+        class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150"
+      >
+        Entrar
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {}
+import { reactive } from 'vue'
+import useModal from '../../hooks/useModal'
+export default {
+  setup () {
+    const modal = useModal()
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+
+      email: {
+        value: '',
+        errorMessage: ''
+      },
+      password: {
+        value: '',
+        errorMessage: ''
+      }
+    })
+    function handleSubmit () {}
+
+    return {
+      state,
+      close: modal.close,
+      handleSubmit
+    }
+  }
+}
 </script>
