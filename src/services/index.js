326580b9de8740b7641c899d2783bfe1ec3d0993
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-02-15 06:25:35 -0300

    feat: service, and axios config

---
+++ src/services/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+import AuthService from './auth'
+
+const API_ENVS = {
+  production: '',
+  development: '',
+  local: 'http://localhost:3000'
+}
+
+const httpClient = axios.create({
+  baseURL: API_ENVS.local
+})
+
+export default {
+  httpClient,
+  auth: AuthService()
+}
